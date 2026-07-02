60b5b699953f1c73f962f16b0aa74f4f832f66bb
luiskione-hub <mayordomomonleon@gmail.com>
2026-07-02 15:53:54 +0200

    Update app 2026-07-02 15:53

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,3 +4,6 @@
 const SUPA_URL  = 'https://jlysauptkssjhnrfxdnq.supabase.co';
 const SUPA_KEY  = 'sb_publishable_9NS_1tbVH4LOpwynGjkmjQ_zWk-oJS-';
 const SUPA_HDR  = {'Content-Type':'application/json','apikey':SUPA_KEY,'Authorization':'Bearer '+SUPA_KEY};
+
+// Local único (TGB + 100M comparten toda la operativa)
+const LOCATION_ID = '1ec1880a-cc48-4770-b71c-6601f0193586';
